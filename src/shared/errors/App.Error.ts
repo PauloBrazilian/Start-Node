7dd109685a6517985a2d32eef6edd6d06435b8f7
Paulo Ricardo <paulo.rdndc@gmail.com>
2024-08-05 14:15:27 -0300

    feat: Add handle errors

---
+++ src/shared/errors/App.Error.ts
@@ -0,0 +1,16 @@
+class AppError{
+
+  public readonly message: string;
+  public readonly stastusCode: number;
+
+  constructor(message: string, statusCode = 400){
+    this.message = message;
+    this.message = message;
+  }
+}
+
+
+
+
+
+export default AppError;
---
+++ src/shared/http/server.ts
@@ -0,0 +1,38 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/App.Error';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+
+    if (error instanceof AppError) {
+      return response.status(error.stastusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+    
+  },
+);
+
+
+app.listen(8090, () => {
+  console.log('Server Start on port 8090')
+})
+
+
+
+
